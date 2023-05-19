c03278b64025f28ae8e4c584efafa8da57f030b2
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-05-19 23:57:08 +0530

    maxLetter done

--- Lab Ques/maxLetter.js
+++ Lab Ques/maxLetter.js
@@ -6,13 +6,13 @@ let maxLengthOfWord = function (words) {
     let maxNumOfLetters = 0;
     let word = "";
 
-    for(let i=0; i<words.length; i++){
-        if(words[i] != " "){
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] != " ") {
             word += words[i];
         }
 
-        if(words[i] == " " || i == words.length - 1){
-            if(word.length > maxNumOfLetters){
+        if (words[i] == " " || i == words.length - 1) {
+            if (word.length > maxNumOfLetters) {
                 maxNumOfLetters = word.length;
             }
             word = "";
