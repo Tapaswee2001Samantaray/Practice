9daa4f54993e6c665d39890183db34082f1bdf04
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-05-19 23:56:19 +0530

    maxLetter done

---
+++ Lab Ques/maxLetter.js
@@ -0,0 +1,25 @@
+/*
+Find maximum number of letters found in a word.
+*/
+
+let maxLengthOfWord = function (words) {
+    let maxNumOfLetters = 0;
+    let word = "";
+
+    for(let i=0; i<words.length; i++){
+        if(words[i] != " "){
+            word += words[i];
+        }
+
+        if(words[i] == " " || i == words.length - 1){
+            if(word.length > maxNumOfLetters){
+                maxNumOfLetters = word.length;
+            }
+            word = "";
+        }
+    }
+    return maxNumOfLetters;
+}
+
+let a = maxLengthOfWord("i love coding");
+console.log(a);
